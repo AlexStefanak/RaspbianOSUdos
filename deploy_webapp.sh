107379a4397cb1192d0f2ec39b0ac4d7b8f16eee
AlexStefanak <stefanak.alex@gmail.com>
2025-05-14 05:59:46 +0200

    Update deploy_webapp.sh

--- deploy_webapp.sh
+++ deploy_webapp.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "📁 Kopírujem webovú stránku do /var/www/html..."
+echo " Kopírujem webovú stránku do /var/www/html..."
 
 sudo cp -r ./webserver/* /var/www/html/
 
-echo "🔧 Nastavujem oprávnenia..."
+echo " Nastavujem oprávnenia..."
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
 
-echo "✅ Webová stránka nasadená. Otvor ju na: http://$(hostname -I | awk '{print $1}')"
+echo " Webová stránka nasadená. Otvor ju na: http://$(hostname -I | awk '{print $1}')"
